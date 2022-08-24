c4fbed470d25c453ac0c9bd9041bd3d4ac7e70be
panosgjuras <panosgjuras@gmail.com>
2022-08-24 16:11:46 +0300

    update of general perceived_choices model

--- Perceived_safety_choice_model.py
+++ Perceived_safety_choice_model.py
@@ -1,22 +1,24 @@
-
 """
 The PERCEIVED SAFETY CHOICE model
 
 @author: ptzouras
 National Technical University of Athens
+Research project: SIM4MTRAN
 """
 import pandas as pd
 import os
 # import numpy as np
+
 # In[00]: Inputs
 current_dir = os.path.dirname(os.path.realpath(__file__)) 
 os.chdir(current_dir)
-b1 = pd.read_csv('raw_data/raw_data_perceived_choices_block1.csv', ',')
+b1 = pd.read_csv('raw_data/raw_data_perceived_choices_block1.csv', ',') # data, as it were downloaded from QuestionPro
 b1["pid"]=range(100,len(b1.index)+100)
 b2 = pd.read_csv('raw_data/raw_data_perceived_choices_block2.csv', ',')
 b2["pid"]=range(200,len(b2.index)+200)
 b3 = pd.read_csv('raw_data/raw_data_perceived_choices_block3.csv', ',') 
 b3["pid"]=range(300,len(b3.index)+300)
+
 # In[01]: Sociodmographic and rating data processing
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.chdir('psafe_models/data_process')
@@ -29,6 +31,7 @@ rate = rate_dats(b1, b2, b3, socio)
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 socio.set_index('pid').to_csv('datasets/socio_dataset_perceived_choices.csv')
 rate.set_index('pid').to_csv('datasets/rating_dataset_perceived_choices.csv') # save the final rating dataset with no correlations
+
 # In[02]: Choice data processing
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.chdir('choice_model/data_process')
@@ -38,3 +41,10 @@ choice = choice_dats(b1, b2, b3, rate, socio)
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 choice.set_index('pid').to_csv('datasets/choice_dataset_perceived_choices.csv')
+
+# In[03]: Development of psafe models
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir('psafe_models')
+
+from psafe_coeff_update import coeff_upd
+coeff = coeff_upd()
