1e205fe788d6b80268b925a28a7955a118dd34ed
panosgjuras <panosgjuras@gmail.com>
2022-11-17 16:30:59 +0200

    Update __init__.py

--- psafechoices/__init__.py
+++ psafechoices/__init__.py
@@ -9,6 +9,7 @@ Research project: SIM4MTRAN
 # from choice_model.data_process.choice_data_process import choice_dats
 # from choice_model.opp_cost_calculator import opp_cost_calc
 # from psafe_models.psafe_coeff_update import coeff_upd
+#
 from .network_analysis.traffic_params_upd import read_shapefile, upd_links
 from .network_analysis.lin_psafe_calc import lin_psafe
 from .network_analysis.shp_to_csv_xml_tool import netcsv_cr, netxml_cr
