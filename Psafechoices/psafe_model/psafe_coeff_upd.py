52d249f6c79a5c570477857e6f05acfba40f562e
panosgjuras <panosgjuras@gmail.com>
2022-11-18 11:13:30 +0200

    create an example scenario + upd psafe model

--- Psafechoices/psafe_model/psafe_coeff_upd.py
+++ Psafechoices/psafe_model/psafe_coeff_upd.py
@@ -3,13 +3,7 @@ import os
 # import numpy as np
 
 def psafe_coeff_upd(model):
-    if model == "simple":
-        coeff = pd.read_csv(os.path.join(os.path.dirname(__file__), 'default_models', 'simple_psafe_models.csv'), sep=',')
-    elif model =="advanced":
-        coeff = pd.read_csv(os.path.join(os.path.dirname(__file__), 'default_models', 'trb_psafe_models.csv'), sep=',')
-    else:
-        coeff = pd.read_csv(model, sep = ',')
-    
+    coeff = pd.read_csv(model, sep = ',')
     coeff = coeff.rename(columns = {'Unnamed: 0':'coeffs'})
     coeff = coeff.set_index('coeffs')
     coeff.loc['kappa.1'] = coeff.loc['kappa.1'] - coeff.loc['constant']
--- scenario_athens/scenario_athens.py
+++ scenario_athens/scenario_athens.py
@@ -1,3 +1,8 @@
+# install Psafechoices package version 0.2
+# pip install git+https://github.com/panosgjuras/Perceived_safety_choices
+
+# upgrade Psafechoices package # STILL UNDER DEVELOPMENT
+# pip install --upgrade --force-reinstall git+https://github.com/panosgjuras/Perceived_safety_choices
 
 import os
 import pandas as pd
@@ -5,17 +10,34 @@ import pandas as pd
 from Psafechoices.network_analysis import traffic_params_upd as trfp
 from Psafechoices.network_analysis import lin_psafe_calc as linpsafe
 from Psafechoices.psafe_model import psafe_coeff_upd as psmodel
-
-# pip install --upgrade --force-reinstall git+https://github.com/panosgjuras/Perceived_safety_choices
+from Psafechoices.network_analysis import shp_to_csv_xml_tool as convert
+from Psafechoices.routing_model import network_graph as dij
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 # read the nodes from a shapefile
 nod = trfp.read_shapefile(os.path.join(root_dir, 'shapefiles', 'experimental_field_athens_nodes.shp'))
 # read the links from a shapefile
+# TEST HERE NEW SCENARIOS WITH INFRASTUCTURE UPDATES
 lin = trfp.read_shapefile(os.path.join(root_dir, 'shapefiles', 'experimental_field_athens_links.shp'))
 # update traffic parameters and coordinates with nodes
 lin = trfp.upd_links(lin, nod)
-# estimate perceived safety model parameters using the output dataset
-cf = psmodel.psafe_coeff_upd('simple')
-
+# estimate perceived safety model parameters using the output model from Rchoice
+# in this case, default perceived safety models are used. User your own models
+cf = psmodel.psafe_coeff_upd(os.path.join(root_dir, 'default_models', 'psafe','simple_psafe_models.csv'))
+# estimate perceived safety per link and per transport mode
 lin = linpsafe.lin_psafe(lin, cf)
+# create a csv file for mapping purposes
+convert.netcsv_cr(lin, os.path.join(root_dir, 'output_csv', 'experimental_field_athens_upd_links.csv'))
+# create an XML for MATSim
+convert.netxml_cr(lin, nod, os.path.join(root_dir, 'output_xml', 'experimental_field_athens_upd_links.xml'))
+
+# run routing algorithm in this network
+origin = 9000 # select origin point
+dest = 4000 # select destination point
+mode = 'escooter' # select transport mode
+method = 'best' # select method, it can be 'shortest' or 'best' path
+minv = 2 # miniumum ACCEPTABLE perceived safety level
+dmin = 100 # in meters minimum distance so that psafe really matters
+coeff = pd.read_csv(os.path.join(root_dir, 'default_models', 'choice', 'coeff_route_model.csv') , sep=',').set_index('param')
+path = dij.dij_run(lin, nod, mode, origin, dest, method, minv, dmin, coeff) # estimate the path
+print(dij.dij_dist_calc (path, lin)) # estimate the path distance
