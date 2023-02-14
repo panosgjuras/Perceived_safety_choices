649dc264fdcd07e354708029745dab5c1f120268
panosgjuras <panosgjuras@gmail.com>
2023-02-14 10:45:47 +0200

    introduce hist plot function of psafe

--- Psafechoices/__init__.py
+++ Psafechoices/__init__.py
@@ -6,14 +6,14 @@ National Technical University of Athens
 Research project: SIM4MTRAN
 """
 from .network_analysis import traffic_params_upd, lin_psafe_calc, shp_to_csv_xml_tool
-from .routing_model import network_graph
+from .routing_model import network_graph, assess_analysis
 from .psafe_model import psafe_coeff_upd as psmod
 from .choice_model import opp_cost_calculator as oppco
 
 __version__ = "0.2" # data processing functions are not included in this version
 __author__ = 'Panagiotis G. Tzouras'
 __all__ = [traffic_params_upd.read_shapefile, traffic_params_upd.upd_links, 
-           lin_psafe_calc.lin_psafe, 
+           lin_psafe_calc.lin_psafe, lin_psafe_calc.psafehist, 
            shp_to_csv_xml_tool.netcsv_cr, shp_to_csv_xml_tool.netxml_cr, 
            network_graph.dij_run, network_graph.dij_dist_calc, psmod.psafe_coeff_upd,
-           oppco.opp_cost_calc]
+           oppco.opp_cost_calc, assess_analysis]
