cbf360775835ca7c06c74d624ba1e8f7ff676d66
panosgjuras <panosgjuras@gmail.com>
2023-02-15 12:54:50 +0200

    no requirements

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name = "psafechoices",
       author_email = "ptzouras@mail.ntua.gr",
       description=" ".join(description.strip().splitlines()),
       packages = find_packages(),
-      install_requires = ['biogeme>=3.2.10', 'dijkstra>=0.2.1', 'lxml>=4.9.1',
-                          'numpy>=1.23.3', 'pandas>=1.5.0', 'pyshp>=2.3.1']
+      
+      # install_requires = ['biogeme>=3.2.10', 'dijkstra>=0.2.1', 'lxml>=4.9.1',
+      #                    'numpy>=1.23.3', 'pandas>=1.5.0', 'pyshp>=2.3.1']
       )
