f81a1e445fdfc85633b5b4bf49aa5b747f3be01e
panosgjuras <63541107+panosgjuras@users.noreply.github.com>
2023-02-16 13:09:14 +0200

    Update __init__.py

--- Psafechoices/__init__.py
+++ Psafechoices/__init__.py
@@ -5,7 +5,7 @@ The PERCEIVED SAFETY CHOICE model
 National Technical University of Athens
 Research project: SIM4MTRAN
 """
-from .network_analysis import traffic_params_upd, lin_psafe_calc, shp_to_csv_xml_tool
+from .network_analysis import traffic_params_upd, lin_psafe_calc, shp_to_csv_xml_tool, maphist
 from .routing_model import network_graph, assess_analysis
 from .psafe_model import psafe_coeff_upd as psmod
 from .choice_model import opp_cost_calculator as oppco
@@ -13,7 +13,7 @@ from .choice_model import opp_cost_calculator as oppco
 __version__ = "0.2" # data processing functions are not included in this version
 __author__ = 'Panagiotis G. Tzouras'
 __all__ = [traffic_params_upd.read_shapefile, traffic_params_upd.upd_links, 
-           lin_psafe_calc.lin_psafe, lin_psafe_calc.psafehist, 
+           lin_psafe_calc.lin_psafe, maphist.psafehist, maphist.psafemap, 
            shp_to_csv_xml_tool.netcsv_cr, shp_to_csv_xml_tool.netxml_cr, 
            network_graph.dij_run, network_graph.dij_dist_calc, psmod.psafe_coeff_upd,
            oppco.opp_cost_calc, assess_analysis]
