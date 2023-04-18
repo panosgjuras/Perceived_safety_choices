2c0e18ab2fc16b04cb945ab7a4586850ef16848e
Laboratory of Transportation Engineering NTUA <121678451+lotentua@users.noreply.github.com>
2023-04-18 13:24:46 +0300

    Update __init__.py

--- Psafechoices/__init__.py
+++ Psafechoices/__init__.py
@@ -12,7 +12,7 @@ from .choice_model import opp_cost_calculator as oppco
 from .MicroIndiAnalysis import indicators
 from .MicroIndiAnalysis import analysistools
 
-__version__ = "0.2" # data processing functions are not included in this version
+__version__ = "0.3" # data processing functions are not included in this version
 __author__ = 'Panagiotis G. Tzouras'
 __all__ = [traffic_params_upd.read_shapefile, traffic_params_upd.upd_links, 
            lin_psafe_calc.lin_psafe, maphist.psafehist, maphist.psafemap, 
