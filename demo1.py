0b01d32ab4cf1fa953eca665e681508f70f3388e
panosgjuras <63541107+panosgjuras@users.noreply.github.com>
2026-04-09 18:18:21 +0300

    upload the new demonstration py files

--- demo1.py
+++ demo1.py
@@ -3,14 +3,15 @@
 """
 @author: panosgtzouras
 """
-
+# import pandas as pd
 import copy
 import os
+# import matplotlib
 
 os.chdir("/Users/panosgtzouras/Desktop/github_tzouras/Perceived_safety_choices/Psafechoices") # TODO: Change it in the final notebook
 from calc import processRowEst
 from mapAnalysis import plotPsafeLev, PsafeHeatmaps
-from mainFuns import modelPsafe_import, linksPsafe_import, score_diff, computeSignificantSafer
+from mainFuns import modelPsafe_import, linksPsafe_import, score_diff
 
 root_dir = "/Users/panosgtzouras/Desktop/github_tzouras/Perceived_safety_choices/data" # TODO: Change it in the final notebook
 
@@ -51,31 +52,25 @@ for m in modes:
     links_1[f'LatPsafe{m}'] = latent_vars
     links_1[f'LevPsafe{m}'] = safety_levels
 
-# TODO: plotting functions are not working using the existing matplotlib version, check the requirements again
-# for m in modes: plotPsafeLev(links_1, m, city = network_city) # If you want to plot the scores for each
-
-# for m in modes: PsafeHeatmaps(links_1, m, city = network_city) # If you want to create a heatmap, showing the density of safe links per mode
+for m in modes: plotPsafeLev(links_1, m, city = network_city) # If you want to plot the scores for each
+for m in modes: PsafeHeatmaps(copy.deepcopy(links_1), m, city = network_city) # If you want to create a heatmap, showing the density of safe links per mode
 
 # %% Step 3.2: Esimtate perceived safety scores based on safety perception of Munich residents
 
-modes = ['car', 'ebike', 'escoot', 'walk']
-
 links_2 = links.copy()
-
 for m in modes:
     latent_vars = []
     safety_levels = []
     
     for index, row in links.iterrows():        
-        latent_vars.append(processRowEst(index, row, modes, cf1)[f'LatPsafe{m}'])
-        safety_levels.append(processRowEst(index, row, modes, cf1)[f'LevPsafe{m}'])
+        latent_vars.append(processRowEst(index, row, modes, cf2)[f'LatPsafe{m}'])
+        safety_levels.append(processRowEst(index, row, modes, cf2)[f'LevPsafe{m}'])
 
     links_2[f'LatPsafe{m}'] = latent_vars
     links_2[f'LevPsafe{m}'] = safety_levels
 
-# for m in modes: plotPsafeLev(links_2, m, city = network_city) # If you want to plot the scores for each
-
-# for m in modes: PsafeHeatmaps(links_2, m, city = network_city) # If you want to create a heatmap, showing the density of safe links per mode
+for m in modes: plotPsafeLev(links_2, m, city = network_city) # If you want to plot the scores for each
+for m in modes: PsafeHeatmaps(copy.deepcopy(links_2), m, city = network_city) # If you want to create a heatmap, showing the density of safe links per mode
 
 # %% Step 4: Run Monte-Carlo Simulation to compare perceptions
 
